b5ff7231429b6d4b05536716e0f190645540e9dd
devansh-299 <f20180131@goa.bits-pilani.ac.in>
2021-05-29 11:58:05 +0530

    project initialized

---
+++ app/src/main/java/com/dev/in/drogon/view/register/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.`in`.drogon.view.register
+
+import android.os.Bundle
+import com.dev.`in`.drogon.R
+import com.dev.`in`.drogon.view.base.BaseActivity
+
+class RegistrationActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
